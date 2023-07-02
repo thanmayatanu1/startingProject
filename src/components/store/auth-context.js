879858a1990cec62ddd42f02bbcf917f3c8079d5
thanmayatanu1 <thanmayatanu1@gmail.com>
2023-07-02 16:02:16 +0530

    Rules of Hooks

--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => {},
+        onLogin: (email, password) => {
+
+        }
+
+    
 });
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+  if(storedUserLoggedInInformation === '1') {
+    setIsLoggedIn(true);
+  }
+
+  }, []);
+
+    const LogoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+      setIsLoggedIn(false);
+    };
+
+    const loginHandler = () => {
+        localStorage.removeItem('isLoggedIn', '1');
+     setIsLoggedIn(true);
+    };
+
+return <AuthContext.Provider value={{
+    isLoggedIn: isLoggedIn,
+     onLogout: LogoutHandler,
+     onLogin: loginHandler,
+      }}>
+    {props.children}
+
+</AuthContext.Provider>
+};
 
 export default AuthContext;
